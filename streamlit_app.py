ea516b419fc5f740ce303a36d5e23a55ae89e9eb
marimahe <151790272+marimahe@users.noreply.github.com>
2024-09-22 20:36:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,14 @@ st.info('this is an experiment')
 #st.snow()
 st.toast("Warming up...")
 st.error("Error message")
+# Insert a chat message container.
+with st.chat_message("user"):
+    st.write("Hello 👋")
+    st.line_chart(np.random.randn(30, 3))
+
+# Display a chat input widget at the bottom of the app.
+>>> st.chat_input("Say something")
+
+# Display a chat input widget inline.
+with st.container():
+    st.chat_input("Say something")
