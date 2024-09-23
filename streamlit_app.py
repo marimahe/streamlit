ac4fb155eee8c04ec11d3995376981f6e7445809
marimahe <151790272+marimahe@users.noreply.github.com>
2024-09-22 20:37:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,9 +13,3 @@ with st.chat_message("user"):
     st.write("Hello 👋")
     st.line_chart(np.random.randn(30, 3))
 
-# Display a chat input widget at the bottom of the app.
-#>>> st.chat_input("Say something")
-
-# Display a chat input widget inline.
-with st.container():
-    st.chat_input("Say something")
