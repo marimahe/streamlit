8287bb91dd2c076e8c02274df0e346a5fd2e0d04
marimahe <151790272+marimahe@users.noreply.github.com>
2024-09-22 20:38:37 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,5 @@ st.info('this is an experiment')
 #st.snow()
 st.toast("Warming up...")
 st.error("Error message")
-# Insert a chat message container.
-with st.chat_message("user"):
-    st.write("Hello 👋")
-    st.line_chart(np.random.randn(30, 3))
+st.button("Click me")
 
